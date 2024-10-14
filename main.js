bd799615e1c1cb71682acf6968680f0741b9fcdb
Augusto Ana <augustora95@gmail.com>
2024-10-14 11:46:05 -0300

    Ejercicio 2.1

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+//Ejercicio 2
 let a = 5;
 let b = 10;
 let c = a+b;
 console.log(c);
 
+//Ejercicio 3
 let nombre = prompt("Ingresa tu nombre");
 console.log(`Hola ${nombre}, bienvenido a la aplicacion`)
+
+//Operadores logicos y condicionales
+//Ejercicio 1
+if(a>b){
+    c = b;
+    console.log(`El mayor de los tres numeros es: ${c}`);
+} else if(b>a) {
+    c = a;
+    console.log(`El mayor de los tres numeros es: ${c}`);
+} else{
+    console.log("Ambos niemeros son iguales"):
+}
